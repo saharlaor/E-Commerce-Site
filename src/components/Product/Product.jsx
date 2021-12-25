e5a9e3ee22091218a5b594ee9a6e68086d3968db
saharoori <saharlaor@gmail.com>
2021-12-25 20:16:45 +0200

    Add to cart button stores in local storage

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,6 +2,13 @@ import React from "react";
 
 import "./Product.css";
 
+const addToCart = (product) => {
+  const products = JSON.parse(localStorage.getItem("products"));
+  const newProducts = products ? [...products, product] : [product];
+  console.log(newProducts);
+  localStorage.setItem("products", JSON.stringify(newProducts));
+};
+
 function Product({ product }) {
   return (
     <div className="Product">
@@ -9,7 +16,7 @@ function Product({ product }) {
       <h4>{product.name}</h4>
       <p>{product.desc}</p>
       <span>{product.price}</span>
-      <button>Add to cart</button>
+      <button onClick={() => addToCart(product)}>Add to cart</button>
     </div>
   );
 }
